e953a5d9a5c22b368e7930e959de1c04aa2db3d3
RASATO12 <rahmatsatrio26@gmail.com>
2025-11-17 15:09:02 +0700

    PercabanganDoWHile

---
+++ ContohDoWhile.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+    <title>Contoh DO WHILE</title>
+</head>
+
+<body>
+    Menggunakan DO WHILE:
+    <br>
+    <?php
+    $jumlah = 10;
+    do {
+        echo $jumlah++;
+        echo "<br>";
+    } while ($jumlah <= 1)
+    ?>
+</body>
+
+</html>
