b88e50952127988ff73e1ccebba6747cff849e8a
Akmal Syarief <akmalsyarief1510@gmail.com>
2025-11-23 21:26:14 +0700

    perulanganataulooping

---
+++ pertemuan9/perulanganataulooping.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+    <title>Perulangan WHILE</title>
+</head>
+
+<body>
+    Menggunakan WHILE<br>
+    <?php
+    $jumlah = 1;
+    while ($i <= 5) {
+        echo $jumlah++;
+        echo "<br>";
+    }
+    ?>
+</body>
+
+</html>
